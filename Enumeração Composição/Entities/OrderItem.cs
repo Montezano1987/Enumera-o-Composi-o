4d3af6bc75828fec48d01f0e6c99c61dc1bb55a4
Bruno Montezano <brunovmontezano@gmail.com>
2024-05-23 11:19:46 -0300

    Arrumando class OrderItem

--- Enumeração Composição/Entities/OrderItem.cs
+++ Enumeração Composição/Entities/OrderItem.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.Text;
 
 namespace Enumeração_Composição.Entities
@@ -8,19 +9,32 @@ namespace Enumeração_Composição.Entities
         public int Quantity { get; set; }
         public double Price { get; set; }
 
+        public Product Product { get; set; }
+
         public OrderItem() 
         { 
         }
 
-        public OrderItem(int quantity, double price)
+        public OrderItem(int quantity, double price, Product product)
         {
             Quantity = quantity;
             Price = price;
+            Product = product;
         }
 
         public double SubTotal()
         { 
             return Quantity * Price;    
+        }
+        public override string ToString()
+        {
+            return Product.Name
+                + ", $"
+                + Price.ToString("F2", CultureInfo.InvariantCulture)
+                + ", Quantity: "
+                + Quantity
+                + ", Subtotal: $"
+                + SubTotal().ToString("F2", CultureInfo.InvariantCulture);
         }
     }
 }
